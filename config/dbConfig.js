e5c5a31152265f23a5d32b16f9ea2dcaba33c6dc
gowtham <gowthamm.5127@gmail.com>
2022-02-06 13:50:29 +0530

    update config file

---
+++ config/dbConfig.js
@@ -0,0 +1,21 @@
+const env = require('dotenv').config();
+const dbName = process.env.DBNAME;
+const userName = process.env.USERNAME;
+const passWord = process.env.PASSWORD;
+const {Sequelize} = require('sequelize');
+
+// const sequelize = new Sequelize('') // Example for postgres
+
+const sequelize = new Sequelize(dbName, userName, passWord, {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+
+// autheticate connection
+sequelize.authenticate()
+.then(() => {
+    console.log("Connection has benn established successfully")
+})
+.catch((err) => {
+    console.log('Unable to connect to database')
+})
